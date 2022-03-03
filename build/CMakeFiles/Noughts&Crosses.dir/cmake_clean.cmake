a145c19b695db458b88ff67b67342973682234a8
Delta Bravo <76425899+DanielButlerIsMyName@users.noreply.github.com>
2022-03-03 15:43:09 +0100

    add function that generates the grid

---
+++ build/CMakeFiles/Noughts&Crosses.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Noughts&Crosses.dir/main.cpp.o"
+  "CMakeFiles/Noughts&Crosses.dir/main.cpp.o.d"
+  "Noughts&Crosses"
+  "Noughts&Crosses.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Noughts&Crosses.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/NoughtsAndCrosses.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NoughtsAndCrosses.dir/main.cpp.o"
+  "CMakeFiles/NoughtsAndCrosses.dir/main.cpp.o.d"
+  "NoughtsAndCrosses"
+  "NoughtsAndCrosses.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NoughtsAndCrosses.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
