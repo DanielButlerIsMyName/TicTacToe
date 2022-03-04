2a7e1e3d0752f64ccb31a0b982468967adbbc056
Delta Bravo <76425899+DanielButlerIsMyName@users.noreply.github.com>
2022-03-04 05:53:26 +0100

    bug fixing

--- main.cpp
+++ main.cpp
@@ -2,16 +2,8 @@
 //  Created by Delta on 28.02.22.
 //
 
-//the following are UBUNTU/LINUX, and MacOS ONLY terminal color codes.
+#include <iostream>
 #define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
 #define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
 #define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
 #define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
@@ -21,23 +13,62 @@
 #define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
 #define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 
-#include <iostream>
+
 using namespace std;
 
-string filds[9] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
-void printGrid(){
-    cout << BOLDGREEN;
-    cout << "  " << BOLDGREEN << filds[0] << RESET << "  |  " << BOLDGREEN << filds[1] << RESET << "  |  " << BOLDGREEN << filds[2] << "\n";
-    cout << RESET << "-----------------\n";
-    cout << "  " << BOLDGREEN << filds[3] << RESET << "  |  " << BOLDGREEN << filds[4] << RESET << "  |  " << BOLDGREEN << filds[5] << "\n";
-    cout << RESET << "-----------------\n";
-    cout << "  " << BOLDGREEN << filds[6] << RESET << "  |  " << BOLDGREEN << filds[7] << RESET << "  |  "<< BOLDGREEN  << filds[8] << BOLDGREEN << "\n";
-    cout << RESET;
+string filds[9] = {"1", "X", "3", "4", "5", "O", "7", "8", "9"};
+bool winner = false
+string turn = "X"
+
+
+void getGridInfo(int x){
+
+    string fildData =  filds[x];
+
+    if (fildData == "X"){
+        cout << BOLDRED << "X" << RESET;
+    }else if (fildData == "O"){
+        cout << BOLDBLUE << "O" << RESET;
+    }else{
+        cout << BOLDGREEN << fildData << RESET;
+    }
+    
+}
+
+void playerAction(){
+    
 }
 
 int main(int argc, const char * argv[]) {
 
-    printGrid();
+    while (winner == false)
+    {
+
+        cout << "  ";
+        getGridInfo(0);
+        cout << "  |  ";
+        getGridInfo(1);
+        cout << "  |  ";
+        getGridInfo(2);
+        cout << "\n";
+        cout << "-----------------\n";
+        cout << "  ";
+        getGridInfo(3);
+        cout << "  |  ";
+        getGridInfo(4);
+        cout << "  |  ";
+        getGridInfo(5);
+        cout << "\n";
+        cout << "-----------------\n";
+        cout << "  ";
+        getGridInfo(6);
+        cout << "  |  ";
+        getGridInfo(7);
+        cout << "  |  ";
+        getGridInfo(8);
+        cout << "\n";
+
+    }
     return 0;
 }
